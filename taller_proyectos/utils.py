3af291c06478c86c27a6ea1b2ff3146ba5681810
GerarD <gerardgun84@gmail.com>
2021-06-05 19:12:50 -0500

    Agregado SQL and print data

--- taller_proyectos/utils.py
+++ taller_proyectos/utils.py
@@ -1,8 +1,9 @@
 import psycopg2
+import re
+import unidecode
 from psycopg2 import Error
 
-
-def test_connection():
+def test_connection(self):
     try:
         # Connect to an existing database
         connection = psycopg2.connect(user="modulo4",
@@ -25,3 +26,32 @@ def test_connection():
             cursor.close()
             connection.close()
             print("PostgreSQL connection is closed")
+
+def get_data():
+    connection = psycopg2.connect(user="modulo4",
+                                  password="modulo4",
+                                  host="128.199.1.222",
+                                  port="5432",
+                                  database="delati")
+
+    cursor = connection.cursor()
+    cursor.execute("""select od.id_ofertadetalle,
+            od.descripcion_normalizada
+            from oferta o
+            inner join oferta_detalle od
+            on (o.id_oferta=od.id_oferta)
+            where  o.id_estado is null and od.ofertaperfil_id=6
+            order by id_ofertadetalle;"""
+           )
+    records = cursor.fetchall()
+    data = []
+    for record in records:
+        # delete tilde
+        text = unidecode.unidecode(record[1].strip())
+        data.append(text.strip())
+
+    for dato in data:
+        print(dato)
+    #
+    #     text = re.sub("[^a-zA-Z0-9]+", "", record[1])
+    # print("You are connected to - ", record, "\n")
